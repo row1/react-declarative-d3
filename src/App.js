d5f4984f98dd2bb72501adc492d53a7df59ee920
Rowan <row1@users.noreply.github.com>
2018-04-21 09:42:49 +1200

    Github pages support

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './views/home';
 import BarChart from './views/bar-chart';
 import DonutChart from './views/donut-chart';
