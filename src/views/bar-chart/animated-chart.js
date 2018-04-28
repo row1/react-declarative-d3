f35373473d21c60baeddc621a303eeabbe66bc1e
Rowan <row1@users.noreply.github.com>
2018-04-28 14:18:10 +1200

    Use a Higher-Order Component for the animation

--- src/views/bar-chart/animated-chart.js
+++ src/views/bar-chart/animated-chart.js
@@ -6,6 +6,8 @@ import { axisBottom, axisLeft } from 'd3-axis';
 import { select } from 'd3-selection';
 import { easeCubicInOut } from 'd3-ease';
 
+import makeAnimated from 'components/makeAnimated';
+
 import dataTsv from './data';
 
 import './chart.css';
@@ -31,82 +33,12 @@ const originalData = tsvParse(dataTsv, d => {
 x.domain(originalData.map(d => d.letter));
 y.domain([0, max(originalData, d => d.frequency)]);
 
-export default class AnimatedBar extends React.Component {
-  constructor() {
-    super();
-    // The normalised time (starts at 0 and goes to 1). See https://github.com/d3/d3-ease#d3-ease
-    const t = 0;
-    this.state = {
-      t,
-      // This starting data will all be zeroed for the start of the animation
-      data: this.easeData(t),
-      // The timestamp of the first animation frame
-      animationStartTime: null,
-      // The timestamp of the last frame -- used to only update per interval
-      animationLastFrameTime: null,
-    };
-  }
-
-  // Basic properties about the animation.
-  // Having a delay is normally a good idea as there will be some skipped frames before the first call to render
-  animation = {
-    duration: 500,
-    delay: 500,
-    interval: 10,
-  };
-
-  animationStep = timestamp => {
-    const { animation } = this;
-    let { animationStartTime, animationLastFrameTime } = this.state;
-
-    if (!animationStartTime) {
-      animationStartTime = timestamp;
-      // Subtract the interval on the first frame so that easeData() will get called below
-      animationLastFrameTime = timestamp - animation.interval;
-    }
-
-    const progress = timestamp - animationStartTime;
-    const t = progress / animation.duration;
-    if (progress < animation.duration) {
-      // Calling easeData() on each frame can be expensive, so only do it per interval
-      const shouldEase =
-        timestamp - animationLastFrameTime >= animation.interval;
-      this.setState({
-        t,
-        data: shouldEase ? this.easeData(t) : this.state.data,
-        animationStartTime,
-        animationLastFrameTime: timestamp,
-      });
-      window.requestAnimationFrame(this.animationStep);
-    } else {
-      this.setState({
-        t: 1,
-        // Ensure that the correct data is used for the final frame.
-        data: originalData,
-      });
-    }
-  };
-
-  // Calculates the data to be used in the current animation frame
-  easeData(t) {
-    return originalData.map(x => ({
-      letter: x.letter,
-      frequency: x.frequency * easeCubicInOut(t),
-    }));
-  }
-  componentDidMount() {
-    const { animation } = this;
-    const start = () => window.requestAnimationFrame(this.animationStep);
-    if (animation.delay) {
-      window.setTimeout(start, animation.delay);
-    } else {
-      start();
-    }
-  }
-  render() {
-    return <Bar data={this.state.data} />;
-  }
-}
+const easeData = (data, t) => {
+  return data.map(x => ({
+    letter: x.letter,
+    frequency: x.frequency * easeCubicInOut(t),
+  }));
+};
 
 const Bar = ({ data }) => (
   <svg width={svgWidth} height={svgHeight}>
@@ -135,3 +67,11 @@ const Bar = ({ data }) => (
     </g>
   </svg>
 );
+
+export default makeAnimated(Bar, {
+  easeData,
+  duration: 500,
+  delay: 500,
+  interval: 10,
+  data: originalData,
+});
